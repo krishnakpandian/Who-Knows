562bb134cf7f316865dda2814a20fc018ee56421
kpandian <kpandian@ucsc.edu>
2020-04-10 21:08:26 -0700

    Made a slight fix

--- findMissing.py
+++ findMissing.py
@@ -10,24 +10,29 @@
 
 
 def findMissing(arr):
+    #print(arr)
     lower = 0               #BST Algorithm
     upper = len(arr) - 1
     middle = (lower + upper) // 2   
     while (lower < upper):  #Checks until they lower and upper are equal
-        #print("Upper:" + str(upper) + " Lower:" + str(lower) + " Middle:" + str(middle))
         middle = (lower + upper) // 2
+        #print("Upper:" + str(upper) + " Lower:" + str(lower) + " Middle:" + str(middle))
         if middle == arr[middle]:   
             lower = middle + 1  #increments lower bound to avoid infinite
         else:
              upper = middle
-             
+
+    middle = (lower+upper) // 2
     if arr[middle] != middle:   #checks if a missing element exists
         return middle
-    raise ValueError(msg ="No missing element")
     return -1
 
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
     #print([1,2,3,4,5])
-    #print(findMissing([0,1,2,3,5,6,7]))
+    arr = list(range(10000000))
+    #print(arr)
+    arr.remove(3)
+    #print(arr)
+    print(findMissing(arr))
 
